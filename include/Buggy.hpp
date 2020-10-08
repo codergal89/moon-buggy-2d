fdba75a206c80d9d4260ae698e99381a187ceb07
Sophia Pearson <codergal89@gmail.com>
2020-10-09 00:51:55 +0200

    buggy: restore feature parity

--- include/Buggy.hpp
+++ include/Buggy.hpp
@@ -5,6 +5,7 @@
 
 #include <Defs.hpp>
 #include <Godot.hpp>
+#include <Physics2DDirectBodyState.hpp>
 #include <RigidBody2D.hpp>
 #include <Vector2.hpp>
 
@@ -16,6 +17,7 @@ namespace moon_buggy
       , TypedNodeCastMixin<Buggy>
   {
     auto static constexpr default_acceleration{.25f};
+    auto static constexpr default_base_speed{0.f};
     auto static constexpr default_drag{.1f};
     auto static constexpr default_jump_velocity{98.f};
     auto static constexpr default_speed_limit{100.f};
@@ -28,19 +30,27 @@ namespace moon_buggy
   private:
     GODOT_CLASS(Buggy, godot::RigidBody2D)  // NOLINT
 
+    auto _integrate_forces(godot::Physics2DDirectBodyState * state) -> void;
+
+    auto update_collision_state(godot::Physics2DDirectBodyState const * state) -> void;
+    auto update_drive_speed() -> void;
+    [[nodiscard]] auto should_jump() const -> bool;
+
     auto stop() -> void;
 
     auto kill_zone_entered(godot::Node * node) -> void;
 
     real_t acceleration{default_acceleration};
+    real_t base_speed{default_base_speed};
     real_t drag{default_drag};
     real_t jump_velocity{default_jump_velocity};
     real_t speed_limit{default_speed_limit};
 
-    godot::Vector2 velocity{};
-    godot::Vector2 gravity{};
+    real_t current_drive_velocity{};
 
     bool can_drive{};
+    bool is_on_floor{};
+    bool bumped_into_something{};
   };
 
 }  // namespace moon_buggy
--- source/Buggy.cpp
+++ source/Buggy.cpp
@@ -2,10 +2,13 @@
 
 #include <AnimatedSprite.hpp>
 #include <Camera2D.hpp>
+#include <CollisionShape2D.hpp>
 #include <Godot.hpp>
 #include <Input.hpp>
 #include <KinematicCollision2D.hpp>
+#include <Physics2DDirectBodyState.hpp>
 #include <ProjectSettings.hpp>
+#include <Shape2D.hpp>
 #include <Sprite.hpp>
 #include <Vector2.hpp>
 
@@ -20,9 +23,11 @@ namespace moon_buggy
   auto Buggy::_register_methods() -> void
   {
     godot::register_method("_ready", &Buggy::_ready);
+    godot::register_method("_integrate_forces", &Buggy::_integrate_forces);
     godot::register_method("kill_zone_entered", &Buggy::kill_zone_entered);
 
     godot::register_property("acceleration", &Buggy::acceleration, default_acceleration);
+    godot::register_property("base_speed", &Buggy::base_speed, default_base_speed);
     godot::register_property("drag", &Buggy::drag, default_drag);
     godot::register_property("jump_velocity", &Buggy::jump_velocity, default_jump_velocity);
     godot::register_property("speed_limit", &Buggy::speed_limit, default_speed_limit);
@@ -32,8 +37,6 @@ namespace moon_buggy
 
   auto Buggy::_init() -> void
   {
-    gravity = godot::ProjectSettings::get_singleton()->get_setting("physics/2d/default_gravity_vector");
-    gravity *= static_cast<real_t>(godot::ProjectSettings::get_singleton()->get_setting("physics/2d/default_gravity"));
   }
 
   auto Buggy::_ready() -> void
@@ -41,10 +44,78 @@ namespace moon_buggy
     can_drive = true;
   }
 
+  auto Buggy::_integrate_forces(godot::Physics2DDirectBodyState * state) -> void
+  {
+    update_collision_state(state);
+
+    update_drive_speed();
+    set_axis_velocity(godot::Vector2{1.f, 0.f} * (current_drive_velocity - base_speed));
+
+    if (should_jump())
+    {
+      set_axis_velocity(godot::Vector2{0.0f, -1.f} * jump_velocity);
+    }
+
+    auto linear_velocity = state->get_linear_velocity();
+    auto step = state->get_step();
+    linear_velocity += state->get_total_gravity() * step;
+    state->set_linear_velocity(linear_velocity);
+  }
+
+  auto Buggy::update_collision_state(godot::Physics2DDirectBodyState const * state) -> void
+  {
+    is_on_floor = false;
+    bumped_into_something = false;
+
+    for (auto i{0}; i < state->get_contact_count(); ++i)
+    {
+      auto contact_normal = state->get_contact_local_normal(i);
+      if (contact_normal.dot({0, -1}) >= 0.9)
+      {
+        is_on_floor = true;
+      }
+      else if (contact_normal.dot({1, 0}) >= 0.9)
+      {
+        bumped_into_something = true;
+      }
+    }
+  }
+
+  auto Buggy::update_drive_speed() -> void
+  {
+    if (is_on_floor)
+    {
+      auto input = godot::Input::get_singleton();
+      auto accelerate = input->is_action_pressed("player_speed_up");
+      auto decelerate = input->is_action_pressed("player_slow_down");
+      auto drive_direction = accelerate * -1 + decelerate * 1;
+
+      if (drive_direction)
+      {
+        current_drive_velocity = std::lerp(current_drive_velocity, static_cast<real_t>(drive_direction) * speed_limit, acceleration);
+      }
+      else
+      {
+        current_drive_velocity = std::lerp(current_drive_velocity, 0.f, drag);
+      }
+    }
+
+    if (bumped_into_something)
+    {
+      current_drive_velocity = 0;
+    }
+  }
+
+  auto Buggy::should_jump() const -> bool
+  {
+    auto input = godot::Input::get_singleton();
+    return is_on_floor && input->is_action_pressed("player_jump");
+  }
+
   auto Buggy::stop() -> void
   {
     can_drive = false;
-    velocity.x = 0.f;
+    set_linear_velocity({0, 0});
   }
 
   auto Buggy::kill_zone_entered(godot::Node * node) -> void
