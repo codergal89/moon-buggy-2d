5185557e6a533912a2754124cdbf95d710533d17
Sophia Pearson <codergal89@gmail.com>
2021-01-16 13:51:32 +0100

    core: add downward direction

---
+++ include/core/Constants.hpp
@@ -0,0 +1,11 @@
+#ifndef MOON_BUGGY_2D_CORE_CONSTANTS_HPP
+#define MOON_BUGGY_2D_CORE_CONSTANTS_HPP
+
+#include <Vector2.hpp>
+
+namespace moon_buggy
+{
+  auto const direction_down = godot::Vector2{0.0f, 1.0f};
+}
+
+#endif
