24ed5bbb56d7cedaeca184f633e3057995c2961e
Sophia Pearson <codergal89@gmail.com>
2021-07-17 10:37:49 +0200

    test: fix main menu tests after refactoring

--- godot/test/unit/test_main_menu.gd
+++ godot/test/unit/test_main_menu.gd
@@ -9,13 +9,13 @@ func test_can_instantiate_main_menu():
 func test_main_menu_has_start_game_signal():
 	assert_has_signal(autofree(MainMenu.new()), "start_game")
 
-func test_invoking_start_button_clicked_emits_start_game():
+func test_invoking_on_start_button_clicked_emits_start_game():
 	var instance: MainMenu = autofree(MainMenu.new())
 	watch_signals(instance)
-	instance.start_button_clicked()
+	instance.on_start_button_clicked()
 	assert_signal_emitted(instance, "start_game")
 
 func test_button_pressed_signal_is_connected_to_start_button_clicked():
 	var scene: MainMenu = add_child_autofree(Scene.instance())
 	var start_button: Button = scene.get_node("RowContainer/StartButton")
-	assert_connected(start_button, scene, "pressed", "start_button_clicked")
+	assert_connected(start_button, scene, "pressed", "on_start_button_clicked")
