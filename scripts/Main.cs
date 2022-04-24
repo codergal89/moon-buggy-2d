8aab645a4f01e8138d0bfcdf095e3015ba9cd058
Sophia Pearson <codergal89@gmail.com>
2022-04-24 10:40:03 +0200

    scenes/Main: add Background script member

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -4,10 +4,12 @@ namespace MoonBuggy2D.scripts
 {
     public class Main : Node
     {
+        public Background Background;
         public Buggy Buggy;
 
         public override void _Ready()
         {
+            Background = GetNode<Background>(nameof(Background));
             Buggy = GetNode<Buggy>(nameof(Buggy));
         }
     }
