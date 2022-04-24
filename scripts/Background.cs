81297276c4ed7647029c6d7eb6965b5d43b678b5
Sophia Pearson <codergal89@gmail.com>
2022-04-24 10:37:50 +0200

    scenes/Background: attach basic script

---
+++ scripts/Background.cs
@@ -0,0 +1,31 @@
+using Godot;
+using MoonBuggy2D.extensions;
+
+namespace MoonBuggy2D.scripts
+{
+    public class Background : ParallaxBackground
+    {
+        public ParallaxLayer Space;
+        public AnimatedSprite SpaceSprite;
+        public ParallaxLayer Stars;
+        public AnimatedSprite StarsSprite;
+
+        public ColorTheme Theme
+        {
+            get => EnumExtensions.ParseEnum<ColorTheme>(SpaceSprite.Animation.ToLower());
+            set
+            {
+                SpaceSprite.Animation = value.Name();
+                StarsSprite.Animation = value.Name();
+            }
+        }
+        
+        public override void _Ready()
+        {
+            Space = GetNode<ParallaxLayer>(nameof(Space));
+            SpaceSprite = Space.GetNode<AnimatedSprite>("Sprite");
+            Stars = GetNode<ParallaxLayer>(nameof(Stars));
+            StarsSprite = Stars.GetNode<AnimatedSprite>("Sprite");
+        }
+    }
+}
