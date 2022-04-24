cc64dc16d65ee19a9a0e0306170225fb903718bc
Sophia Pearson <codergal89@gmail.com>
2022-04-24 08:48:49 +0200

    scenes/Buggy: add initial Buggy scene

---
+++ extensions/EnumExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace MoonBuggy2D.extensions
+{
+    public static class EnumExtensions
+    {
+        #region Private Methods
+
+        private static void EnsureIsEnum<T>() where T : struct
+        {
+            if (!typeof(T).IsEnum)
+                throw new InvalidOperationException("Generic type argument is not an enum");
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static string Name<T>(this T animation) where T : struct
+        {
+            EnsureIsEnum<T>();
+            return animation.ToString().ToLower();
+        }
+
+        public static T ParseEnum<T>(string value) where T : struct
+        {
+            EnsureIsEnum<T>();
+            return (T)Enum.Parse(typeof(T), value);
+        }
+
+        #endregion
+    }
+}
