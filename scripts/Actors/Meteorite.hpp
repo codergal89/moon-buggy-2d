0242c05650130f52fca0376f89b0401ca0458cfa
Sophia Pearson <codergal89@gmail.com>
2023-03-06 08:31:31 +0100

    scripts: clean up Meteorite implementation

--- scripts/Actors/Meteorite.hpp
+++ scripts/Actors/Meteorite.hpp
@@ -1,13 +1,14 @@
 #ifndef MB2D_SCRIPTS_ACTORS_METEORITE_HPP
 #define MB2D_SCRIPTS_ACTORS_METEORITE_HPP
 
-#include "godot_cpp/classes/animated_sprite2d.hpp"
-#include "godot_cpp/classes/collision_shape2d.hpp"
-#include "godot_cpp/classes/rigid_body2d.hpp"
-#include "godot_cpp/classes/visible_on_screen_notifier2d.hpp"
+#include <godot_cpp/classes/animated_sprite2d.hpp>
+#include <godot_cpp/classes/collision_shape2d.hpp>
+#include <godot_cpp/classes/rigid_body2d.hpp>
+#include <godot_cpp/classes/visible_on_screen_notifier2d.hpp>
 #include <godot_cpp/classes/engine.hpp>
 
-#include "godot_cpp/core/class_db.hpp"
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/error_macros.hpp>
 
 namespace mb2d
 {
@@ -16,7 +17,7 @@ namespace mb2d
     {
         auto static constexpr cStartingVelocity = 100.0;
 
-        GDCLASS(Meteorite, godot::RigidBody2D);
+        GDCLASS(Meteorite, godot::RigidBody2D)
 
         static auto _bind_methods() -> void
         {
@@ -29,11 +30,19 @@ namespace mb2d
             impacted_sprite = get_node<godot::AnimatedSprite2D>("%ImpactedSprite");
             visibility_notifier = get_node<godot::VisibleOnScreenNotifier2D>("%VisibilityNotifier");
 
-            if (godot::Engine::get_singleton()->is_editor_hint())
-            {
-                return;
-            }
+            ERR_FAIL_NULL(collision_shape);
+            ERR_FAIL_NULL(flying_sprite);
+            ERR_FAIL_NULL(impacted_sprite);
+            ERR_FAIL_NULL(visibility_notifier);
 
+            if (!godot::Engine::get_singleton()->is_editor_hint())
+            {
+                ready_in_game();
+            }
+        }
+
+        auto ready_in_game() -> void
+        {
             flying_sprite->play();
             impacted_sprite->set_visible(false);
 
